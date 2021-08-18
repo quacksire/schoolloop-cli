c675678fe628b0802ec35abda3391622fce56dda
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-08-17 19:01:48 -0700

    push

--- index.js
+++ index.js
@@ -2,115 +2,43 @@
 
 let axios = require('axios');
 let os = require('os')
-const ora = require('ora');
-const { AutoComplete, BasicAuth, Invisible, Input} = require('enquirer');
-
+//const ora = require('ora');
+//const { AutoComplete, BasicAuth, Invisible, Input } = require('enquirer');
+var base64 = require('base-64');
 var urlencode = require('urlencode');
-let school;
+
+let pack = module.exports
+
 
 // Litterly any token works
-        let token = os.release()
-        let devOS = os.type()
-        let year = new Date().getFullYear()
+let token = os.release()
+let devOS = os.type()
+let year = new Date().getFullYear()
 
-class schoolLoop {
-    constructor() {
-    
-        console.log('New School created')
-    
-    }
-    login() {
-    
-        console.clear()
-        const spinner = ora('Getting list of schools').start();
-        let schoolList = axios('https://anything-can-go-here.schoolloop.com/mapi/schools')
-            .then(function (response) {
-                //console.log(response.data)
-                let schoolNames = new Array()
-                response.data.forEach(school => {
-                    schoolNames.push(school.name)
-                });
-                //console.log(schoolList[0])
-                spinner.stop()
-                const schoolSelector = new AutoComplete({
-                    name: 'schools',
-                    message: 'School Name',
-                    limit: 1,
-                    initial: 0,
-                    choices: schoolNames
-                });
-                schoolSelector.run()
-                    .then(answer => {
-                        //Get School Object
-                        let counter = 0
-                        while (true) {
-                            if (response.data[counter].name == answer) {
-                                break
-                            }
-                            counter++
-                        }
-                        school = response.data[counter]
-                        //console.log(school)
-                        let username
-                        let password
-                        const usernamePrompt = new Input({
-                            message: `Please enter your username to log into ${school.name}`,
-                            initial: 'jonschlinkert'
-                        });
-                        usernamePrompt.run().then(answer => {
-                  
-                            username = answer
-                            const passwordPrompt = new Invisible({
-                                message: `Password?`
-                            });
-                            passwordPrompt.run().then(answer => {
-                                password = answer
-                                axios(`https://${school.domainName}/mapi/login?version=3&devToken=${token}&devOS=${devOS}&year=${year}`, {
-                                    headers: {
-                                        authorization: `${urlencode(username)}:${urlencode(password)}`,
-                                    }
-                                })
-                                    .then(function (response) {
-                                        console.log(response.data.data)
-                                    })
-                                    //////////////////////
-                                    .catch(function (err) {
-                                        console.error(`Login Failed \n ${err.response.data}`)
-                          
-                                    })
-                  
-                            }).catch(console.log);
-                        }).catch(console.log);
-                    })
-                    .catch(console.error);
-                this.auth = `${urlencode(username)}:${urlencode(password)}`
-            });
-}
+pack.schoolList = () => {
+
+    axios('https://anything-can-go-here.schoolloop.com/mapi/schools')
+        .then(function(response) {
+        
+            return response.data
+            
+        })
+
 }
 
-
-new schoolLoop().login()
-
+pack.user = (slDomain, slUsername, slPassword) => {
 
 
-
-//console.log(os.type())
-//"https://\(domainName)/mapi/login?version=3&devToken=\(SchoolLoopConstants.devToken)&devOS=\(SchoolLoopConstants.devOS)&year=\(SchoolLoopConstants.year)
-
-/*
-axios(`https://${school.domainName}/login?version=3&devToken=${token}&devOS=${devOS}&year=${year}`{
-  headers: {
-    authorization: 'test-value'
-  })
-  .then(function (response) {
-     console.log(response.data)
-  })
-    .catch(function (err) {
-    console.error(err)
+    let auth = `${urlencode(username)}:${urlencode(password)}`
+    auth = base64.encode(auth)
+    axios(`https://${school.domainName}/mapi/login?version=3&devToken=${token}&devOS=${devOS}&year=${year}`, {
+        headers: {
+            Authorization: `Basic ${auth}`,
+        }
+    })
+    .then(function (response) {
+        return response
     })
 
-*/
-//https://hmbhs.schoolloop.com/mapi/report_card?studentID=70532
-
-
+}
 
